30760a10fa385d6c93db54fe9595338cf706fb00
Jessegao2020 <66278615+Jessegao2020@users.noreply.github.com>
2026-09-10 10:56:47 +0800

    Enable article save only after edits

--- Desktop/MiniERP.Desktop.Avalonia/Views/Articles/ArticleEditorView.axaml.cs
+++ Desktop/MiniERP.Desktop.Avalonia/Views/Articles/ArticleEditorView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using MiniERP.Desktop.Controls;
 using MiniERP.Desktop.Infrastructure;
@@ -11,11 +12,14 @@ namespace MiniERP.Desktop.Views.Articles;
 public partial class ArticleEditorView : UserControl
 {
     private readonly DecimalTextBoxFilter _priceFilter;
+    private bool _trackDirtyChanges;
     private ArticleEditorViewModel ViewModel => (ArticleEditorViewModel)DataContext!;
 
     public event EventHandler? Saved;
     public event EventHandler? Deleted;
     public event EventHandler? RequestClose;
+
+    public int ArticleId => ViewModel.Article.Id;
 
     public ArticleEditorView(Article? article)
     {
@@ -24,10 +28,23 @@ public partial class ArticleEditorView : UserControl
         var settings = App.Services.GetRequiredService<AppSettingsService>();
         DataContext = new ArticleEditorViewModel(article, settings);
         _priceFilter = new DecimalTextBoxFilter(PriceTextBox);
+
+        AttachedToVisualTree += (_, _) =>
+            Dispatcher.UIThread.Post(() =>
+            {
+                ViewModel.MarkClean();
+                _trackDirtyChanges = true;
+            }, DispatcherPriority.Loaded);
     }
 
     public void RefreshExchangeRate()
         => ViewModel.RefreshExchangeRate();
+
+    private void EditorField_TextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (_trackDirtyChanges)
+            ViewModel.MarkDirty();
+    }
 
     private void GridView_Click(object? sender, RoutedEventArgs e)
         => RequestClose?.Invoke(this, EventArgs.Empty);
@@ -37,7 +54,6 @@ public partial class ArticleEditorView : UserControl
         if (!await ViewModel.SaveAsync()) return;
 
         Saved?.Invoke(this, EventArgs.Empty);
-        RequestClose?.Invoke(this, EventArgs.Empty);
     }
 
     private void Discard_Click(object? sender, RoutedEventArgs e)
