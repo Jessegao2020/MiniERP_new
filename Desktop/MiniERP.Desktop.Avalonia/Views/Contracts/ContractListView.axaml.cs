67bdf85c380e700f556729dfb76c63d5535c01a5
Jessegao2020 <66278615+Jessegao2020@users.noreply.github.com>
2026-09-10 09:00:46 +0800

    Wire SelectLine contract grid behavior

--- Desktop/MiniERP.Desktop.Avalonia/Views/Contracts/ContractListView.axaml.cs
+++ Desktop/MiniERP.Desktop.Avalonia/Views/Contracts/ContractListView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
 using MiniERP.Desktop.Infrastructure;
 using MiniERP.Desktop.ViewModels.Contracts;
 using MiniERP.Domain;
@@ -9,6 +10,7 @@ namespace MiniERP.Desktop.Views.Contracts;
 
 public partial class ContractListView : UserControl
 {
+    private readonly SelectLineSortState _sortState = new();
     private ContractListViewModel ViewModel => (ContractListViewModel)DataContext!;
 
     public event Action<Contract?>? OpenContractRequested;
@@ -17,10 +19,19 @@ public partial class ContractListView : UserControl
     {
         InitializeComponent();
         DataContext = new ContractListViewModel();
-        AttachedToVisualTree += async (_, _) => await ViewModel.LoadAsync();
+        AttachedToVisualTree += async (_, _) =>
+        {
+            await ViewModel.LoadAsync();
+            ApplySort();
+            Dispatcher.UIThread.Post(SyncDataGridColumnWidths, DispatcherPriority.Loaded);
+        };
     }
 
-    public Task ReloadAsync() => ViewModel.LoadAsync();
+    public async Task ReloadAsync()
+    {
+        await ViewModel.LoadAsync();
+        ApplySort();
+    }
 
     private void New_Click(object? sender, RoutedEventArgs e) => OpenContractRequested?.Invoke(null);
 
@@ -31,12 +42,58 @@ public partial class ContractListView : UserControl
         if (confirmed) await ViewModel.DeleteSelectedAsync();
     }
 
-    private async void Refresh_Click(object? sender, RoutedEventArgs e) => await ViewModel.LoadAsync();
+    private async void Refresh_Click(object? sender, RoutedEventArgs e)
+    {
+        await ViewModel.LoadAsync();
+        ApplySort();
+    }
 
     private void Filter_TextChanged(object? sender, TextChangedEventArgs e)
     {
-        if (sender is TextBox textBox && textBox.Tag is string field) ViewModel.SetFilter(field, textBox.Text);
+        if (sender is TextBox textBox && textBox.Tag is string field)
+        {
+            ViewModel.SetFilter(field, textBox.Text);
+            ApplySort();
+        }
     }
+
+    private void SortHeader_Tapped(object? sender, TappedEventArgs e)
+    {
+        if (sender is not Border border || border.Tag is not string field) return;
+        _sortState.Toggle(field);
+        ApplySort();
+        UpdateSortIndicators();
+    }
+
+    private void ApplySort()
+    {
+        if (_sortState.Field is { } field)
+            SelectLineGridSupport.SortInPlace(ViewModel.Contracts, field, _sortState.Ascending);
+    }
+
+    private void UpdateSortIndicators()
+    {
+        NumberSortArrow.Text = _sortState.Arrow("ContractNumber");
+        CustomerSortArrow.Text = _sortState.Arrow("CustomerNameSnapshot");
+        UserSortArrow.Text = _sortState.Arrow("SalesContactNameSnapshot");
+        DateSortArrow.Text = _sortState.Arrow("ContractDate");
+        PoSortArrow.Text = _sortState.Arrow("CustomerPoNumber");
+        CurrencySortArrow.Text = _sortState.Arrow("Currency");
+        TotalSortArrow.Text = _sortState.Arrow("TotalAmount");
+        SourceSortArrow.Text = _sortState.Arrow("SourceDocumentNumber");
+    }
+
+    private void TableLayout_SizeChanged(object? sender, SizeChangedEventArgs e)
+        => Dispatcher.UIThread.Post(SyncDataGridColumnWidths, DispatcherPriority.Render);
+
+    private void ColumnSplitter_DragDelta(object? sender, VectorEventArgs e)
+        => Dispatcher.UIThread.Post(SyncDataGridColumnWidths, DispatcherPriority.Render);
+
+    private void SyncDataGridColumnWidths()
+        => SelectLineGridSupport.SyncColumnWidths(ContractTableLayout, ContractGrid);
+
+    private void Grid_LoadingRow(object? sender, DataGridRowEventArgs e)
+        => SelectLineGridSupport.ApplyAlternateRow(e);
 
     private void ContractGrid_DoubleTapped(object? sender, TappedEventArgs e)
     {
